2c2fc27eefaee12966a299bd5cedf28d426d7c0b
Rathalin <danielflo1997@hotmail.com>
2023-04-29 16:18:30 +0200

    Improve main page

--- my-quiz-app/src/pages/index.page.tsx
+++ my-quiz-app/src/pages/index.page.tsx
@@ -1,18 +1,39 @@
+import GradientWord from '@/components/GradientWord';
 import LinkButton from '@/components/LinkButton';
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <Box sx={{ marginTop: 4 }}>
-      <LinkButton
-        hrefObserver="/create/1-general"
-        navigateOnClick
-        iconSide="right"
-        variant="contained"
+    <Box>
+      <Typography
+        component="h1"
+        sx={{
+          fontSize: '4rem',
+          marginBlock: 4,
+          display: 'flex',
+          justifyContent: 'center',
+        }}
       >
-        Create a new quiz
-      </LinkButton>
+        <GradientWord>Quizio</GradientWord>
+      </Typography>
+      <Box
+        sx={{
+          marginTop: 4,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <LinkButton
+          hrefObserver="/create/1-general"
+          navigateOnClick
+          iconSide="right"
+          variant="contained"
+        >
+          Create a new quiz
+        </LinkButton>
+      </Box>
     </Box>
   );
 }
