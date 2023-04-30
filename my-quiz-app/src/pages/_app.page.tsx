97677798f2347dbdcfd1d5a8e0a4c104dbb95e6a
Rathalin <danielflo1997@hotmail.com>
2023-04-30 14:00:30 +0200

    Add react query devtools

--- my-quiz-app/src/pages/_app.page.tsx
+++ my-quiz-app/src/pages/_app.page.tsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { usePageTransition } from '@/stores/page-transition.store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -49,6 +50,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ThemeProvider>
   );
