825d654fbaa4a0d22f556b043e0d99925c981edd
Rathalin <danielflo1997@hotmail.com>
2023-05-09 20:41:00 +0200

    Config modularizeImports

--- my-quiz-app/next.config.js
+++ my-quiz-app/next.config.js
@@ -2,14 +2,12 @@
 const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts'],
   reactStrictMode: true,
-  experimental: {
-    modularizeImports: {
-      '@mui/material': {
-        transform: '@mui/material/{{member}}',
-      },
-      '@mui/icons-material': {
-        transform: '@mui/icons-material/{{member}}',
-      },
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
     },
   },
 };
