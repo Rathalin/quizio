67eab48bc1a361283645d42e6dcae0f2f786764a
Rathalin <danielflo1997@hotmail.com>
2023-05-09 20:43:57 +0200

    Remove mui from modularizeImports

--- my-quiz-app/next.config.js
+++ my-quiz-app/next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts'],
   reactStrictMode: true,
   modularizeImports: {
-    '@mui/material': {
-      transform: '@mui/material/{{member}}',
-    },
     '@mui/icons-material': {
       transform: '@mui/icons-material/{{member}}',
     },
