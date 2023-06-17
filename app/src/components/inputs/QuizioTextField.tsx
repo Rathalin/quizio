b579750eb84f3fa7fc8ec5970e44e7d00939bbe9
Rathalin <danielflo1997@hotmail.com>
2023-06-17 11:23:53 +0200

    Add forwardRef to QuizioTextField

--- app/src/components/inputs/QuizioTextField.tsx
+++ app/src/components/inputs/QuizioTextField.tsx
@@ -4,41 +4,51 @@ import {
   TextField,
   TextFieldProps,
 } from '@mui/material';
-import { useMemo } from 'react';
+import { forwardRef, useMemo } from 'react';
 
 type QuizioTextInputProps = TextFieldProps & {};
 
-export default function QuizioTextField({
-  helperText,
-  value,
-  inputProps,
-  fullWidth,
-  sx,
-  ...props
-}: QuizioTextInputProps) {
-  const remainingCharacters = useMemo(() => {
-    if (inputProps?.maxLength == null) return null;
-    return `${(value as any)?.length ?? 0}/${inputProps?.maxLength}`;
-  }, [value, inputProps?.maxLength]);
+export default forwardRef<HTMLDivElement, QuizioTextInputProps>(
+  function QuizioTextField(
+    {
+      helperText,
+      value,
+      inputProps,
+      fullWidth,
+      sx,
+      ...other
+    }: QuizioTextInputProps,
+    ref
+  ) {
+    const remainingCharacters = useMemo(() => {
+      if (inputProps?.maxLength == null) return null;
+      return `${(value as any)?.length ?? 0}/${inputProps?.maxLength}`;
+    }, [value, inputProps?.maxLength]);
 
-  return (
-    <Stack
-      sx={{ display: 'inline-flex', width: fullWidth ? '100%' : 'auto', ...sx }}
-    >
-      <TextField
-        value={value}
-        inputProps={inputProps}
-        fullWidth={fullWidth}
-        {...props}
-      />
+    return (
       <Stack
-        direction="row"
-        justifyContent="space-between"
-        sx={{ marginInline: 1 }}
+        sx={{
+          display: 'inline-flex',
+          width: fullWidth ? '100%' : 'auto',
+          ...sx,
+        }}
       >
-        <FormHelperText error>{helperText}</FormHelperText>
-        <FormHelperText>{remainingCharacters}</FormHelperText>
+        <TextField
+          value={value}
+          inputProps={inputProps}
+          fullWidth={fullWidth}
+          ref={ref}
+          {...other}
+        />
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          sx={{ marginInline: 1 }}
+        >
+          <FormHelperText error>{helperText}</FormHelperText>
+          <FormHelperText>{remainingCharacters}</FormHelperText>
+        </Stack>
       </Stack>
-    </Stack>
-  );
-}
+    );
+  }
+);
