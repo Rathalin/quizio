0ea6e33c06df90a1fdce32db607edd01ee685456
Rathalin <danielflo1997@hotmail.com>
2023-06-19 20:43:06 +0200

    Increase question count limit to 100 in GQL

--- app/src/graphql/quizzes.ts
+++ app/src/graphql/quizzes.ts
@@ -12,7 +12,7 @@ export const getAllPublishedQuizzesGQL = graphql(`
           published
           createdAt
           playCount
-          questions {
+          questions(pagination: { limit: 100 }) {
             data {
               id
             }
