1dbe82893d11930b7f5112c5e3665afad7377344
Rathalin <danielflo1997@hotmail.com>
2023-06-22 16:06:48 +0200

    Add copy link to quiz button

--- app/src/components/QuizOverview.tsx
+++ app/src/components/QuizOverview.tsx
@@ -1,8 +1,12 @@
 import {
+  Alert,
   Box,
   Card,
   CardContent,
   Chip,
+  IconButton,
+  Snackbar,
+  Tooltip,
   Typography,
   darken,
   lighten,
@@ -13,9 +17,10 @@ import {
   BarChart as BarChartIcon,
   Image as ImageIcon,
   PlayArrow as PlayArrowIcon,
+  Share as ShareIcon,
 } from '@mui/icons-material';
 import LinkButton from './LinkButton';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { usePrefersLightMode } from '@/custom-hooks/usePrefersLightMode';
 
 type QuizOverviewProps = {
@@ -46,120 +51,157 @@ export default function QuizOverview({
   const theme = useTheme();
   const prefersLightMode = usePrefersLightMode();
   const isQuestionCountSingular = questionCount === 1;
+  const [copiedText, setCopiedText] = useState<string | null>(null);
+  const showCopiedAlert = copiedText != null;
 
   const dateFormat = useMemo(
     () => new Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }),
     []
   );
 
+  function handleShareClick() {
+    navigator.clipboard.writeText(`${window.location.origin}/play/${uuid}`);
+    const titleLimit = 30;
+    const titleText =
+      title.length > titleLimit ? `${title.slice(0, titleLimit)}...` : title;
+    setCopiedText(`Copied link to '${titleText}'`);
+  }
+
+  function handleCopiedAlertClose(_event: unknown, reason?: string) {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setCopiedText(null);
+  }
+
   return (
-    <Card
-      elevation={2}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    >
-      {imageUrl != null ? (
-        <Image
-          loader={({ src, width: _width, quality: _quality }) =>
-            `${process.env.NEXT_PUBLIC_BACKEND_URL}${src}`
-          }
-          src={imageUrl}
-          alt="QuizImage"
-          width={300}
-          height={200}
-          style={{
-            objectFit: 'cover',
-            width: '100%',
-            minHeight: '180px',
-            filter: prefersLightMode ? 'opacity(0.8)' : 'none',
-          }}
-          priority
-        ></Image>
-      ) : (
-        <Box
-          sx={{
-            minHeight: '180px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: prefersLightMode
-              ? lighten(theme.palette.secondary.light, 0.4)
-              : darken(theme.palette.secondary.dark, 0.4),
-          }}
-        >
-          <ImageIcon fontSize="large" />
-        </Box>
-      )}
-      <CardContent
+    <>
+      <Snackbar
+        open={showCopiedAlert}
+        anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
+        autoHideDuration={2000}
+        onClose={handleCopiedAlertClose}
+      >
+        <Alert severity="info" onClose={handleCopiedAlertClose}>
+          {copiedText}
+        </Alert>
+      </Snackbar>
+      <Card
+        elevation={2}
         sx={{
-          height: '100%',
           display: 'flex',
           flexDirection: 'column',
-          justifyContent: 'space-between',
         }}
       >
-        <Box>
-          <Box>
-            <Typography
-              variant="h3"
-              component="h2"
-              sx={{ display: 'flex', justifyContent: 'space-between' }}
-            >
-              {title}
-              {/* {isMyQuiz && (
-                <Box sx={{ justifySelf: 'center' }}>
-                  <Link href={`/quiz/edit/${uuid}`}>
-                    <Tooltip title="Edit your quiz" arrow>
-                      <IconButton>
-                        <EditIcon color="secondary" />
-                      </IconButton>
-                    </Tooltip>
-                  </Link>
-                </Box>
-              )} */}
-            </Typography>
+        {imageUrl != null ? (
+          <Image
+            loader={({ src, width: _width, quality: _quality }) =>
+              `${process.env.NEXT_PUBLIC_BACKEND_URL}${src}`
+            }
+            src={imageUrl}
+            alt="QuizImage"
+            width={300}
+            height={200}
+            style={{
+              objectFit: 'cover',
+              width: '100%',
+              minHeight: '180px',
+              filter: prefersLightMode ? 'opacity(0.8)' : 'none',
+            }}
+            priority
+          ></Image>
+        ) : (
+          <Box
+            sx={{
+              minHeight: '180px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: prefersLightMode
+                ? lighten(theme.palette.secondary.light, 0.4)
+                : darken(theme.palette.secondary.dark, 0.4),
+            }}
+          >
+            <ImageIcon fontSize="large" />
           </Box>
-          <Box>
-            <Typography sx={{ marginBottom: '.8rem' }}>
-              {description}
-            </Typography>
-            <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap' }}>
-              <Chip label={username} variant="filled" />
-              <Chip
-                label={`${questionCount} question${
-                  isQuestionCountSingular ? '' : 's'
-                }`}
-                variant="outlined"
-              />
-              <Chip
-                label={playCount}
-                icon={<BarChartIcon fontSize="small" />}
-                variant="outlined"
-              />
-              <Chip label={dateFormat.format(createdAt)} variant="outlined" />
-            </Box>
-          </Box>
-        </Box>
-        <Box
+        )}
+        <CardContent
           sx={{
-            marginTop: 4,
+            height: '100%',
             display: 'flex',
-            justifyContent: 'center',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'center',
           }}
         >
-          <LinkButton
-            hrefObserver={`/play/${uuid}`}
-            navigateOnClick
-            variant="contained"
-            size="large"
-            endIcon={<PlayArrowIcon />}
+          <Box>
+            <Box>
+              <Typography
+                variant="h3"
+                component="h2"
+                sx={{ display: 'flex', justifyContent: 'space-between' }}
+              >
+                {title}
+                {/* {isMyQuiz && (
+                  <Box sx={{ justifySelf: 'center' }}>
+                    <Link href={`/quiz/edit/${uuid}`}>
+                      <Tooltip title="Edit your quiz" arrow>
+                        <IconButton>
+                          <EditIcon color="secondary" />
+                        </IconButton>
+                      </Tooltip>
+                    </Link>
+                  </Box>
+                )} */}
+                <Box>
+                  <Tooltip title="Copy link" arrow>
+                    <IconButton onClick={() => handleShareClick()}>
+                      <ShareIcon color="secondary" />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+              </Typography>
+            </Box>
+            <Box>
+              <Typography sx={{ marginBottom: '.8rem' }}>
+                {description}
+              </Typography>
+              <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap' }}>
+                <Chip label={username} variant="filled" />
+                <Chip
+                  label={`${questionCount} question${
+                    isQuestionCountSingular ? '' : 's'
+                  }`}
+                  variant="outlined"
+                />
+                <Chip
+                  label={playCount}
+                  icon={<BarChartIcon fontSize="small" />}
+                  variant="outlined"
+                />
+                <Chip label={dateFormat.format(createdAt)} variant="outlined" />
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              marginTop: 4,
+              display: 'flex',
+              justifyContent: 'center',
+            }}
           >
-            Play
-          </LinkButton>
-        </Box>
-      </CardContent>
-    </Card>
+            <LinkButton
+              hrefObserver={`/play/${uuid}`}
+              navigateOnClick
+              variant="contained"
+              size="large"
+              endIcon={<PlayArrowIcon />}
+            >
+              Play
+            </LinkButton>
+          </Box>
+        </CardContent>
+      </Card>
+    </>
   );
 }
