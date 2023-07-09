4696168cd873f411589adda6ef2f3fafaa0a5e16
Rathalin <danielflo1997@hotmail.com>
2023-07-09 17:59:10 +0200

    Add useHandleGQLUnauthorized hook

---
+++ app/src/custom-hooks/useHandleGQLUnauthorized.ts
@@ -0,0 +1,28 @@
+import { signOut } from 'next-auth/react';
+import { useEffect } from 'react';
+import { z } from 'zod';
+
+export function useHandleGQLUnauthorized(errors: unknown[]) {
+  useEffect(() => {
+    try {
+      const gqlErrors = GqlErrorsSchema.parse(errors);
+      gqlErrors.forEach((e) => {
+        if (e != null && e.response.status === 401) {
+          signOut();
+        }
+      });
+    } catch (e) {
+      console.log('useHandleGQLUnauthorized', e);
+    }
+  }, [errors]);
+}
+
+const GqlErrorsSchema = z.array(
+  z
+    .object({
+      response: z.object({
+        status: z.number(),
+      }),
+    })
+    .nullable()
+);
