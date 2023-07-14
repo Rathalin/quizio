f1ce23d3c0b00971d101f0d351fb6914289f4afb
Rathalin <danielflo1997@hotmail.com>
2023-07-14 19:50:03 +0200

    Fix missing style for OverviewFormPlaceholder

--- app/src/page-components/quiz/edit/OverviewFormPlaceholder.tsx
+++ app/src/page-components/quiz/edit/OverviewFormPlaceholder.tsx
@@ -1,12 +1,16 @@
 import PlaceholderBox from '@/components/placeholders/PlaceholderBox';
-import { Stack } from '@mui/material';
+import { Card, CardContent, Stack } from '@mui/material';
 
 export default function OverviewFormPlaceholder() {
   return (
-    <Stack alignItems="start" gap={6}>
-      <PlaceholderBox minHeight="3rem" sx={{ width: '100%' }} />
-      <PlaceholderBox minHeight="3rem" sx={{ width: '100%' }} />
-      <PlaceholderBox minHeight="200px" minWidth="300px" />
-    </Stack>
+    <Card>
+      <CardContent>
+        <Stack alignItems="start" gap={6}>
+          <PlaceholderBox minHeight="3rem" sx={{ width: '100%' }} />
+          <PlaceholderBox minHeight="3rem" sx={{ width: '100%' }} />
+          <PlaceholderBox minHeight="200px" minWidth="300px" />
+        </Stack>
+      </CardContent>
+    </Card>
   );
 }
