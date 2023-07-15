d49beb6bf6ea1539db6e754933df0b25719b91ad
Rathalin <danielflo1997@hotmail.com>
2023-07-15 19:12:08 +0200

    Improve alert image layout

--- app/src/components/AlertsViewer.tsx
+++ app/src/components/AlertsViewer.tsx
@@ -47,7 +47,7 @@ export default function AlertsViewer() {
               <Grid
                 item
                 xs={12}
-                sm={8}
+                sm={12}
                 md={9}
                 lg={10}
                 sx={{
@@ -58,8 +58,12 @@ export default function AlertsViewer() {
               >
                 <ReactMarkdown>{alert.attributes?.content ?? ''}</ReactMarkdown>
               </Grid>
-              <Grid item xs={12} sm={4} md={3} lg={2}>
-                <Stack justifyContent="center" sx={{ height: '100%' }}>
+              <Grid item xs={12} sm={12} md={3} lg={2}>
+                <Stack
+                  justifyContent="center"
+                  alignItems="center"
+                  sx={{ height: '100%' }}
+                >
                   {alert.attributes?.image?.data?.attributes?.url != null && (
                     <Image
                       src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${alert.attributes.image.data.attributes.url}`}
@@ -77,6 +81,7 @@ export default function AlertsViewer() {
                       style={{
                         objectFit: 'cover',
                         filter: mode === 'light' ? 'opacity(0.8)' : 'none',
+                        borderRadius: 2,
                       }}
                       unoptimized
                     />
@@ -93,17 +98,18 @@ export default function AlertsViewer() {
 
 type ImageSize = `${Enum_Alert_Imagesize}`;
 
+const imageAspectRatio = 16 / 9;
 const imageSizes = {
   small: {
-    width: 60,
+    width: 60 * imageAspectRatio,
     height: 60,
   },
   medium: {
-    width: 90,
+    width: 90 * imageAspectRatio,
     height: 90,
   },
   large: {
-    width: 120,
+    width: 120 * imageAspectRatio,
     height: 120,
   },
 } as const satisfies Record<ImageSize, { width: number; height: number }>;
