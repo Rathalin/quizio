c64c93879faa4922b002d17a9bee711135bc2d6d
Rathalin <danielflo1997@hotmail.com>
2023-07-16 15:49:50 +0200

    Remove expired query param from url after alert close

--- app/src/page-components/dashboard/InvalidTokenAlert.tsx
+++ app/src/page-components/dashboard/InvalidTokenAlert.tsx
@@ -1,8 +1,20 @@
 import { Alert, AlertTitle, Box, Collapse } from '@mui/material';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 export default function InvalidTokenAlert() {
+  const router = useRouter();
   const [open, setOpen] = useState(true);
+
+  function removeSessionExpiredQueryParam() {
+    const { pathname, query } = router;
+    delete query.sessionExpired;
+
+    router.replace({
+      pathname,
+      query,
+    });
+  }
 
   return (
     <Collapse in={open}>
@@ -10,7 +22,10 @@ export default function InvalidTokenAlert() {
         variant="filled"
         severity="error"
         sx={{ marginBottom: 4 }}
-        onClose={() => setOpen(false)}
+        onClose={() => {
+          setOpen(false);
+          removeSessionExpiredQueryParam();
+        }}
       >
         <AlertTitle>Your session has expired!</AlertTitle>
         <Box>Please log in again.</Box>
