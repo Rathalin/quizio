eea5fd5603bf41dd0790e18885496754389a0431
Rathalin <danielflo1997@hotmail.com>
2023-07-16 11:35:09 +0200

    Add session alert to useHandleGQLUnauthorized

--- app/src/custom-hooks/useHandleGQLUnauthorized.ts
+++ app/src/custom-hooks/useHandleGQLUnauthorized.ts
@@ -1,20 +1,22 @@
 import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { z } from 'zod';
 
 export function useHandleGQLUnauthorized(errors: unknown[]) {
+  const router = useRouter();
   useEffect(() => {
     try {
       const gqlErrors = GqlErrorsSchema.parse(errors);
       gqlErrors.forEach((e) => {
         if (e != null && e.response.status === 401) {
-          signOut();
+          signOut().then(() => router.push('/?sessionExpired=true'));
         }
       });
     } catch (e) {
       console.error('useHandleGQLUnauthorized', e);
     }
-  }, [errors]);
+  }, [errors, router]);
 }
 
 const GqlErrorsSchema = z.array(
