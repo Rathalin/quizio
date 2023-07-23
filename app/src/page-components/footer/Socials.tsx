ebac22a4723db5978f99a474a143157ae861f1c0
Rathalin <danielflo1997@hotmail.com>
2023-07-23 12:46:27 +0200

    Add flockert.at to footer socials

--- app/src/page-components/footer/Socials.tsx
+++ app/src/page-components/footer/Socials.tsx
@@ -1,28 +1,34 @@
-import { GitHub, LinkedIn, Twitter } from '@mui/icons-material';
-import { Stack, useTheme } from '@mui/material';
+import { AccountBox, GitHub, LinkedIn, Twitter } from '@mui/icons-material';
+import { Box, Stack, useTheme } from '@mui/material';
 import Link from 'next/link';
-import { ElementType } from 'react';
+import { PropsWithChildren } from 'react';
 
 export default function Socials() {
   return (
     <Stack direction="row" gap={2} flexWrap="wrap">
-      <SocialIcon href="https://github.com/rathalin/" icon={GitHub} />
-      <SocialIcon
-        href="https://www.linkedin.com/in/daniel-flockert-63ba26201/"
-        icon={LinkedIn}
-      />
-      <SocialIcon href="https://twitter.com/Rathalin" icon={Twitter} />
+      <SocialIcon href="https://flockert.at/">
+        <AccountBox />
+      </SocialIcon>
+      <SocialIcon href="https://github.com/rathalin/">
+        <GitHub />
+      </SocialIcon>
+      <SocialIcon href="https://www.linkedin.com/in/daniel-flockert-63ba26201/">
+        <LinkedIn />
+      </SocialIcon>
+      <SocialIcon href="https://twitter.com/Rathalin">
+        <Twitter />
+      </SocialIcon>
     </Stack>
   );
 }
 
-function SocialIcon({ href, icon: Icon }: { href: string; icon: ElementType }) {
+function SocialIcon({ href, children }: PropsWithChildren<{ href: string }>) {
   const theme = useTheme();
   const hoverColor = theme.palette.primary.main;
 
   return (
     <Link href={href}>
-      <Icon sx={{ '&:hover': { color: hoverColor } }} />
+      <Box sx={{ '&:hover': { color: hoverColor } }}>{children}</Box>
     </Link>
   );
 }
