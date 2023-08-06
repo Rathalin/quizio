60b19b50b20556d668c1826d6c9a3ad9fcd2cf1c
Rathalin <danielflo1997@hotmail.com>
2023-08-06 19:04:26 +0200

    Improve 500 page

--- app/src/pages/500.page.tsx
+++ app/src/pages/500.page.tsx
@@ -1,8 +1,12 @@
-import GenericLoadingErrorMessage from '@/components/GenericLoadingErrorMessage';
 import LinkButton from '@/components/LinkButton';
-import { Box, Typography } from '@mui/material';
+import { useColorMode } from '@/page-components/theme.context';
+import { Box, Stack, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function InternalServerErrorPage() {
+  const { mode } = useColorMode();
+
   return (
     <Box
       sx={{
@@ -12,21 +16,35 @@ export default function InternalServerErrorPage() {
         justifyContent: 'center',
       }}
     >
-      <Box>
-        <Typography variant="h1">
-          <GenericLoadingErrorMessage />
-        </Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <LinkButton
-            hrefObserver="/"
-            navigateOnClick
-            variant="contained"
-            iconSide="left"
-          >
-            Home
-          </LinkButton>
+      <Stack alignItems="center">
+        <Typography variant="h1">Something broke.</Typography>
+        <Box sx={{ marginBottom: 4 }}>
+          <Image
+            src="/images/CatChewingOnWire.jpg"
+            alt="Cat chewing on wire"
+            width={300}
+            height={200}
+            style={{
+              borderRadius: 2,
+              objectFit: 'cover',
+              filter: mode === 'dark' ? 'brightness(0.8)' : 'none',
+            }}
+          />
         </Box>
-      </Box>
+        <Typography sx={{ marginBottom: 4 }}>
+          <span>{'Please contact '}</span>
+          <Link href="mailto:daniel@flockert">daniel@flockert.at</Link>
+          <span>{', if the error persists.'}</span>
+        </Typography>
+        <LinkButton
+          hrefObserver="/"
+          navigateOnClick
+          variant="contained"
+          iconSide="left"
+        >
+          Home
+        </LinkButton>
+      </Stack>
     </Box>
   );
 }
