849d43fbd3b4de2fcc76c16776d94e3bf76f2aa7
Rathalin <danielflo1997@hotmail.com>
2023-10-08 10:52:49 +0200

    Increase error message contrast

--- app/src/components/GenericLoadingErrorMessage.tsx
+++ app/src/components/GenericLoadingErrorMessage.tsx
@@ -1,22 +1,24 @@
-import { Alert, Box } from '@mui/material';
+import { Alert, Box, Typography } from '@mui/material';
 import Link from 'next/link';
 
 export default function GenericLoadingErrorMessage() {
   return (
-    <Alert severity="error" sx={{ marginTop: 2 }}>
-      <Box component="span">
-        {
-          'Sorry, the Quizio services are currently unavailable. If the error persists, contact '
-        }
-      </Box>
-      <Link
-        href="mailto:daniel@flockert.at"
-        style={{ fontWeight: 700 }}
-        className="no-underline"
-      >
-        daniel@flockert.at
-      </Link>
-      <Box component="span">.</Box>
+    <Alert severity="error" variant="filled" sx={{ marginTop: 2 }}>
+      <Typography variant="body1" component="div" sx={{ marginBottom: 0 }}>
+        <Box component="span">
+          {
+            'Sorry, the Quizio services are currently unavailable. If the error persists, contact '
+          }
+        </Box>
+        <Link
+          href="mailto:daniel@flockert.at"
+          style={{ fontWeight: 700 }}
+          className="no-underline"
+        >
+          daniel@flockert.at
+        </Link>
+        <Box component="span">.</Box>
+      </Typography>
     </Alert>
   );
 }
