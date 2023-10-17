17c4847ec869ca7fb740d3e3b522185e117b8e91
Rathalin <danielflo1997@hotmail.com>
2023-10-17 19:26:08 +0200

    Fix image input bugs

--- app/src/page-components/quiz/create/QuestionsForm.tsx
+++ app/src/page-components/quiz/create/QuestionsForm.tsx
@@ -27,6 +27,7 @@ import { DevTool } from '@hookform/devtools';
 import { isBrowser } from '@/utilities/isBrowser';
 import { FormErrorIcon } from '../FormErrorIcon';
 import AddIcon from '@mui/icons-material/Add';
+import { raise } from '@/utilities/errorHandling';
 
 type QuestionsFormProps = {
   defaultData: QuizQuestionsForm;
@@ -57,7 +58,7 @@ export default function QuestionsForm({
     getValues,
     reset,
     watch,
-    formState: { isValid, errors },
+    formState: { isValid },
   } = methods;
   const { fields, append, remove } = useFieldArray({
     name: 'questions',
@@ -86,22 +87,52 @@ export default function QuestionsForm({
 
   useEffect(() => {
     if (editMode) return;
-    reset(getStorageItem() as QuizQuestionsForm);
-  }, [editMode, getStorageItem, reset]);
+    // Keep image from form data
+    const questions = getValues('questions');
+    const storageData = getStorageItem();
+    if (storageData != null) {
+      for (
+        let questionIndex = 0;
+        questionIndex < questions.length;
+        questionIndex++
+      ) {
+        const storageQuestion =
+          storageData.questions.at(questionIndex) ??
+          raise(`No question at index ${questionIndex} in storage data`);
+        const questionImage = questions.at(questionIndex)?.questionImage;
+        if (questionImage != null && storageQuestion.questionImage != null) {
+          storageQuestion.questionImage = questionImage;
+        }
+        const explanationImage = questions.at(questionIndex)?.explanationImage;
+        if (
+          explanationImage != null &&
+          storageQuestion.explanationImage != null
+        ) {
+          storageQuestion.explanationImage = explanationImage;
+        }
+      }
+    }
+    reset(storageData as QuizQuestionsForm);
+  }, [editMode, getStorageItem, getValues, reset]);
+
   useEffect(() => {
     if (editMode) return;
     const subscription = watch((value) => {
+      const valueToStore = structuredClone(value);
       // Don't store question and explanation images
-      for (const question of value.questions ?? []) {
-        if (question != null) {
-          // delete question.questionImage;
-          // delete question.explanationImage;
+      for (const question of valueToStore.questions ?? []) {
+        if (question?.questionImage?.data?.file != null) {
+          question.questionImage.data.file = null;
+        }
+        if (question?.explanationImage?.data?.file != null) {
+          question.explanationImage.data.file = null;
         }
       }
-      setStorageItem(value as QuizQuestionsForm);
+      setStorageItem(valueToStore as QuizQuestionsForm);
     });
     return () => subscription.unsubscribe();
-  }, [watch, setStorageItem, editMode]);
+  }, [watch, setStorageItem, editMode, getValues]);
+
   useEffect(() => {
     if (!editMode) return;
     reset(defaultData);
@@ -171,14 +202,8 @@ export default function QuestionsForm({
                   <Typography color="error">
                     This form still contains errors!
                   </Typography>
-                  <Typography color="error">
-                    {JSON.stringify(errors)}
-                  </Typography>
                 </Stack>
               )}
-              <Typography color="error">
-                {JSON.stringify(watch(`questions.0`))}
-              </Typography>
               <Stack
                 direction="row"
                 justifyContent="space-between"
