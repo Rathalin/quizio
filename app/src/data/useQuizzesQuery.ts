53c848600f89ac3ed1d346966e1e9cceeaef9bb8
Rathalin <danielflo1997@hotmail.com>
2025-01-21 15:32:19 +0100

    Remove auth header from getQuizzes

--- app/src/data/useQuizzesQuery.ts
+++ app/src/data/useQuizzesQuery.ts
@@ -5,26 +5,19 @@ import {
   InferFetchResult,
   throwOnError,
 } from '@/api-client';
-import {
-  AuthorizationHeader,
-  useAuthHeader,
-} from '@/custom-hooks/useAuthHeader';
 import { seconds } from '@/utilities/time';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 export function useQuizzesInfiniteQuery(
   query: Omit<GetQuizzesRequestQuery, 'page'>
 ) {
-  const authHeader = useAuthHeader();
   return useInfiniteQuery<
     InferFetchResult<typeof fetchQuizzes>,
     InferFetchError<typeof fetchQuizzes>
   >({
     queryKey: ['getQuizzesInfinite', query],
     queryFn: ({ pageParam = 0 }) =>
-      throwOnError(() =>
-        fetchQuizzes({ ...query, page: pageParam }, authHeader)
-      ),
+      throwOnError(() => fetchQuizzes({ ...query, page: pageParam })),
     getNextPageParam: ({ meta: { page, totalPages } }, _pages) => {
       if (page < totalPages) {
         return page + 1;
@@ -35,14 +28,10 @@ export function useQuizzesInfiniteQuery(
   });
 }
 
-async function fetchQuizzes(
-  query: GetQuizzesRequestQuery,
-  authHeader: AuthorizationHeader | undefined
-) {
+async function fetchQuizzes(query: GetQuizzesRequestQuery) {
   return client.GET('/quizzes', {
     params: {
       query,
     },
-    headers: authHeader,
   });
 }
