0deede6696b9bf27030f810f5ae309dc213101f4
Rathalin <danielflo1997@hotmail.com>
2025-02-19 21:06:48 +0100

    Use pulse instead of wave for skeletons

--- app/src/theme.ts
+++ app/src/theme.ts
@@ -104,13 +104,6 @@ export const theme = responsiveFontSizes(
         textTransform: 'none',
       },
     },
-    components: {
-      MuiSkeleton: {
-        defaultProps: {
-          animation: 'wave',
-        },
-      },
-    },
   }),
 );
 
