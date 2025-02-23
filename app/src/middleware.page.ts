aaca71b10fc28859679560bf2b74a00ca6c2b8c7
Rathalin <danielflo1997@hotmail.com>
2025-02-23 21:44:05 +0100

    Consider locale in middleware

--- app/src/middleware.page.ts
+++ app/src/middleware.page.ts
@@ -1,10 +1,15 @@
-import { withAuth } from 'next-auth/middleware';
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware';
+import { NextFetchEvent } from 'next/server';
 
-export default withAuth({
-  pages: {
-    signIn: '/auth/signin',
-  },
-});
+export default function middleware(req: NextRequestWithAuth, res: NextFetchEvent) {
+  const locale = req.nextUrl.locale ?? 'de';
+
+  return withAuth({
+    pages: {
+      signIn: `/${locale}/auth/signin`,
+    },
+  })(req, res);
+}
 
 export const config = {
   matcher: ['/users/me', '/users/me/change-password', '/my-quizzes', '/quiz/create', '/quiz/edit/:path'],
