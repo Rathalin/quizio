44062486d51c92262ee74fbc5dece02287995aa9
Rathalin <danielflo1997@hotmail.com>
2025-07-16 23:24:11 +0200

    Rename single letter variables

--- backend/main.go
+++ backend/main.go
@@ -35,78 +35,78 @@ func main() {
 
 	response.DefaultErrorResponseContentType = "application/problem+json"
 
-	r := openapi3.NewReflector()
-	s := web.NewService(r)
+	reflector := openapi3.NewReflector()
+	service := web.NewService(reflector)
 
-	s.OpenAPISchema().SetTitle("Quizzes API")
-	s.OpenAPISchema().SetDescription("This service manages quizzes and their questions.")
-	s.OpenAPISchema().SetVersion("v1.0.0")
-	s.OpenAPISchema().SetHTTPBearerTokenSecurity("JWT token", "baerer", "")
+	service.OpenAPISchema().SetTitle("Quizzes API")
+	service.OpenAPISchema().SetDescription("This service manages quizzes and their questions.")
+	service.OpenAPISchema().SetVersion("v1.0.0")
+	service.OpenAPISchema().SetHTTPBearerTokenSecurity("JWT token", "baerer", "")
 
-	s.Use(
+	service.Use(
 		cors.AllowAll().Handler,
 	)
 
 	// Public routes
-	s.Group(func(r chi.Router) {
-		r.Handle("/public/*", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+	service.Group(func(router chi.Router) {
+		router.Handle("/public/*", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 
-		r.Method(http.MethodPost, "/register", nethttp.NewHandler(dbWrapper.Register()))
-		r.Method(http.MethodPost, "/refresh-token", nethttp.NewHandler(dbWrapper.RefreshToken()))
-		r.Method(http.MethodPost, "/signin", nethttp.NewHandler(dbWrapper.SignIn()))
-		r.Method(http.MethodGet, "/allowed-file-types", nethttp.NewHandler(dbWrapper.GetAllowedFileTypes()))
-		r.Method(http.MethodGet, "/quizzes", nethttp.NewHandler(dbWrapper.GetQuizzes()))
-		r.Method(http.MethodGet, "/play/{uuid}", nethttp.NewHandler(dbWrapper.PlayQuiz()))
-		r.Method(http.MethodGet, "/user-profile/{uuid}", nethttp.NewHandler(dbWrapper.GetPublicUserProfile()))
-		r.Method(http.MethodGet, "/alerts", nethttp.NewHandler(dbWrapper.GetAlerts()))
-		r.Method(http.MethodPost, "/play-protocol-entry", nethttp.NewHandler((dbWrapper.CreatePlayProtocolEntry())))
+		router.Method(http.MethodPost, "/register", nethttp.NewHandler(dbWrapper.Register()))
+		router.Method(http.MethodPost, "/refresh-token", nethttp.NewHandler(dbWrapper.RefreshToken()))
+		router.Method(http.MethodPost, "/signin", nethttp.NewHandler(dbWrapper.SignIn()))
+		router.Method(http.MethodGet, "/allowed-file-types", nethttp.NewHandler(dbWrapper.GetAllowedFileTypes()))
+		router.Method(http.MethodGet, "/quizzes", nethttp.NewHandler(dbWrapper.GetQuizzes()))
+		router.Method(http.MethodGet, "/play/{uuid}", nethttp.NewHandler(dbWrapper.PlayQuiz()))
+		router.Method(http.MethodGet, "/user-profile/{uuid}", nethttp.NewHandler(dbWrapper.GetPublicUserProfile()))
+		router.Method(http.MethodGet, "/alerts", nethttp.NewHandler(dbWrapper.GetAlerts()))
+		router.Method(http.MethodPost, "/play-protocol-entry", nethttp.NewHandler((dbWrapper.CreatePlayProtocolEntry())))
 	})
 
 	// Auth routes
-	s.Route("/user", func(r chi.Router) {
-		r.With(
-			nethttp.HTTPBearerSecurityMiddleware(s.OpenAPICollector, "JWT token", "baerer", "string"),
-		).Group(func(r chi.Router) {
-			r.Use(
+	service.Route("/user", func(router chi.Router) {
+		router.With(
+			nethttp.HTTPBearerSecurityMiddleware(service.OpenAPICollector, "JWT token", "baerer", "string"),
+		).Group(func(router chi.Router) {
+			router.Use(
 				jwtauth.Verifier(auth.TokenAuth),
 				jwtauth.Authenticator(auth.TokenAuth),
 			)
-			r.Method(http.MethodPost, "/upload", nethttp.NewHandler((dbWrapper.UploadFile())))
-			r.Method(http.MethodDelete, "/upload", nethttp.NewHandler((dbWrapper.DeleteFile())))
-			r.Method(http.MethodPost, "/signout", nethttp.NewHandler(dbWrapper.HandleSignOut()))
-			r.Method(http.MethodGet, "/my-quizzes", nethttp.NewHandler((dbWrapper.GetMyQuizzes())))
-			r.Method(http.MethodGet, "/quiz/{uuid}/trends", nethttp.NewHandler((dbWrapper.GetMyQuizTrends())))
-			r.Method(http.MethodPost, "/quiz/create", nethttp.NewHandler((dbWrapper.CreateQuiz())))
-			r.Method(http.MethodGet, "/quiz/{uuid}", nethttp.NewHandler(dbWrapper.GetQuiz()))
-			r.Method(http.MethodPost, "/quiz/{uuid}", nethttp.NewHandler((dbWrapper.UpdateQuiz())))
-			r.Method(http.MethodPost, "/quiz/{uuid}/visibility", nethttp.NewHandler((dbWrapper.UpdateQuizVisibility())))
-			r.Method(http.MethodDelete, "/quiz/{uuid}", nethttp.NewHandler((dbWrapper.DeleteQuiz())))
-			r.Method(http.MethodPost, "/play-protocol-entry", nethttp.NewHandler((dbWrapper.CreatePlayProtocolEntryWithUser())))
-			r.Method(http.MethodGet, "/user-account", nethttp.NewHandler(dbWrapper.GetUserAccount()))
-			r.Method(http.MethodGet, "/my-user-profile", nethttp.NewHandler(dbWrapper.GetMyUserProfile()))
-			r.Method(http.MethodPost, "/change-password", nethttp.NewHandler(dbWrapper.ChangePassword()))
-			r.Method(http.MethodPost, "/update-profile-image", nethttp.NewHandler(dbWrapper.UpdateUserProfileImage()))
+			router.Method(http.MethodPost, "/upload", nethttp.NewHandler((dbWrapper.UploadFile())))
+			router.Method(http.MethodDelete, "/upload", nethttp.NewHandler((dbWrapper.DeleteFile())))
+			router.Method(http.MethodPost, "/signout", nethttp.NewHandler(dbWrapper.HandleSignOut()))
+			router.Method(http.MethodGet, "/my-quizzes", nethttp.NewHandler((dbWrapper.GetMyQuizzes())))
+			router.Method(http.MethodGet, "/quiz/{uuid}/trends", nethttp.NewHandler((dbWrapper.GetMyQuizTrends())))
+			router.Method(http.MethodPost, "/quiz/create", nethttp.NewHandler((dbWrapper.CreateQuiz())))
+			router.Method(http.MethodGet, "/quiz/{uuid}", nethttp.NewHandler(dbWrapper.GetQuiz()))
+			router.Method(http.MethodPost, "/quiz/{uuid}", nethttp.NewHandler((dbWrapper.UpdateQuiz())))
+			router.Method(http.MethodPost, "/quiz/{uuid}/visibility", nethttp.NewHandler((dbWrapper.UpdateQuizVisibility())))
+			router.Method(http.MethodDelete, "/quiz/{uuid}", nethttp.NewHandler((dbWrapper.DeleteQuiz())))
+			router.Method(http.MethodPost, "/play-protocol-entry", nethttp.NewHandler((dbWrapper.CreatePlayProtocolEntryWithUser())))
+			router.Method(http.MethodGet, "/user-account", nethttp.NewHandler(dbWrapper.GetUserAccount()))
+			router.Method(http.MethodGet, "/my-user-profile", nethttp.NewHandler(dbWrapper.GetMyUserProfile()))
+			router.Method(http.MethodPost, "/change-password", nethttp.NewHandler(dbWrapper.ChangePassword()))
+			router.Method(http.MethodPost, "/update-profile-image", nethttp.NewHandler(dbWrapper.UpdateUserProfileImage()))
 		})
 	})
 
-	s.Route("/seo", func(r chi.Router) {
-		r.With(nethttp.HTTPBearerSecurityMiddleware(s.OpenAPICollector, "SEO API Key", "baerer", "string")).Group(func(r chi.Router) {
+	service.Route("/seo", func(router chi.Router) {
+		router.With(nethttp.HTTPBearerSecurityMiddleware(service.OpenAPICollector, "SEO API Key", "baerer", "string")).Group(func(r chi.Router) {
 			r.Use(middlewares.APIKeyMiddleware(env.Config.SEOAPIKey))
 			r.Method(http.MethodGet, "/published-quizzes-uuids", nethttp.NewHandler(dbWrapper.PublishedQuizzesUuids()))
 		})
 	})
 
 	docsAuth := middleware.BasicAuth("Docs Access", map[string]string{env.Config.OpenAPIDocsUser: env.Config.OpenAPIDocsPassword})
-	docsSecuritySchema := nethttp.HTTPBasicSecurityMiddleware(s.OpenAPICollector, "Docs Access", "Basic authentication for accessing the OpenAPI docs")
-	s.Route("/docs", func(r chi.Router) {
-		r.Group(func(r chi.Router) {
-			r.Use(docsAuth, docsSecuritySchema)
+	docsSecuritySchema := nethttp.HTTPBasicSecurityMiddleware(service.OpenAPICollector, "Docs Access", "Basic authentication for accessing the OpenAPI docs")
+	service.Route("/docs", func(r chi.Router) {
+		r.Group(func(router chi.Router) {
+			router.Use(docsAuth, docsSecuritySchema)
 			if env.Config.GoEnv != "local" {
-				r.Method(http.MethodGet, "/openapi.json", s.OpenAPICollector)
+				router.Method(http.MethodGet, "/openapi.json", service.OpenAPICollector)
 			}
 			// Serve the Swagger UI
-			r.Mount("/", v5emb.New(
-				s.OpenAPISchema().Title(),
+			router.Mount("/", v5emb.New(
+				service.OpenAPISchema().Title(),
 				"/docs/openapi.json",
 				"/docs",
 			))
@@ -114,15 +114,15 @@ func main() {
 	})
 
 	if env.Config.GoEnv == "local" {
-		s.Method(http.MethodGet, "/docs/openapi.json", s.OpenAPICollector)
+		service.Method(http.MethodGet, "/docs/openapi.json", service.OpenAPICollector)
 	}
 
-	s.Route("/", func(r chi.Router) {
+	service.Route("/", func(r chi.Router) {
 		r.Method(http.MethodGet, "/", http.RedirectHandler("/docs", http.StatusMovedPermanently))
 	})
 
 	log.Println("Starting service")
-	if err := http.ListenAndServe("0.0.0.0:8080", s); err != nil {
+	if err := http.ListenAndServe("0.0.0.0:8080", service); err != nil {
 		log.Fatal(err)
 	}
 }
