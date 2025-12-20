f157a6fa31648b164828f16d04f22a95a71284fb
Rathalin <danielflo1997@hotmail.com>
2025-12-20 20:14:06 +0100

    FIx eslinit

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -1,29 +1,23 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
 import unusedImports from 'eslint-plugin-unused-imports';
-import react from 'eslint-plugin-react';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import nextTs from 'eslint-config-next/typescript';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default defineConfig([
-  globalIgnores(['**/temp.js', '**/generated/*', '**/env.d.ts', '**/next-sitemap.config.js']),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
   {
-    extends: compat.extends('next', 'prettier'),
-
     plugins: {
       'unused-imports': unusedImports,
-      react,
     },
-
     rules: {
       '@next/next/no-html-link-for-pages': 'off',
       'unused-imports/no-unused-imports': 'error',
@@ -48,3 +42,5 @@ export default defineConfig([
     },
   },
 ]);
+
+export default eslintConfig;
